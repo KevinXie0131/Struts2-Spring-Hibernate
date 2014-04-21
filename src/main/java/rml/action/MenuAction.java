0bdfd2ab16f6d3ab8f4772835894cbe8173bc0b6
zhibingXie <zhibing.x@gmail.com>
2014-04-21 14:20:26 -0400

    menu action

--- src/main/java/rml/action/MenuAction.java
+++ src/main/java/rml/action/MenuAction.java
@@ -2,20 +2,32 @@ package rml.action;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import rml.pageModel.Menu;
+import rml.model.vo.Json;
+import rml.model.vo.Menu;
 import rml.service.MenuServiceI;
+import rml.util.ExceptionUtil;
 
 import com.opensymphony.xwork2.ModelDriven;
 
-@Action(value = "menuAction")
+@Namespace("/")
+@Action(value = "menuAction", 
+		results = { 
+			@Result(name = "menu", location = "/demo/admin/menu.jsp"), 
+		})
 public class MenuAction extends BaseAction implements ModelDriven<Menu> {
-   
+
 	private static final Logger logger = Logger.getLogger(MenuAction.class);
 
 	private Menu menu = new Menu();
 
+	public Menu getModel() {
+		return menu;
+	}
+	
 	private MenuServiceI menuService;
 
 	public MenuServiceI getMenuService() {
@@ -26,23 +38,27 @@ public class MenuAction extends BaseAction implements ModelDriven<Menu> {
 	public void setMenuService(MenuServiceI menuService) {
 		this.menuService = menuService;
 	}
-
-	@Override
-	public Menu getModel() {
-		return menu;
+	
+	public String menu() {
+		return "menu";
 	}
 
 	/**
-	 * get tree node asynchronous 
-	 * @throws Exception 
+	 * get tree of home menu
 	 */
-	public void getTreeNode() {
-		super.writeJson(menuService.getTreeNode(menu.getId()));
+	public void doNotNeedSession_tree() {
+		super.writeJson(menuService.tree(menu, false));
 	}
-	
-	//revise json, make icon display
-	public void getAllTreeNode() {
-		super.writeJson_icon(menuService.getAllTreeNode());
+
+	public void doNotNeedSession_treeRecursive() {
+		super.writeJson(menuService.tree(menu, true));
+	}
+
+	/**
+	 * get treegrid of menu
+	 */
+	public void treegrid() {
+		super.writeJson(menuService.treegrid(menu));
 	}
 
 }
