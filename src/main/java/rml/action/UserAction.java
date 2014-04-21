82f00c2412cdda3fb637bebc945892245ee3a42a
zhibingXie <zhibing.x@gmail.com>
2014-04-21 14:20:34 -0400

    user action

--- src/main/java/rml/action/UserAction.java
+++ src/main/java/rml/action/UserAction.java
@@ -1,40 +1,45 @@
 package rml.action;
 
-import java.util.Date;
-import java.util.UUID;
-
-import javax.servlet.ServletContext;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.*;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import rml.model.Tuser;
-import rml.pageModel.Json;
-import rml.pageModel.User;
-import rml.service.TestService;
+import rml.model.vo.Json;
+import rml.model.vo.RoleChart;
+import rml.model.vo.SessionInfo;
+import rml.model.vo.User;
 import rml.service.UserServiceI;
-
+import rml.util.ChartUtils;
+import rml.util.ExceptionUtil;
+import rml.util.IpUtil;
+import rml.util.ResourceUtil;
 
 import com.opensymphony.xwork2.ModelDriven;
 
 @Namespace("/")
-@Action(value = "userAction")
+@Action(value = "userAction", 
+		results = { 
+			@Result(name = "user", location = "/admin/user.jsp"), 
+			@Result(name = "userAdd", location = "/admin/userAdd.jsp"), 
+			@Result(name = "userEdit", location = "/admin/userEdit.jsp"), 
+			@Result(name = "userRoleEdit", location = "/admin/userRoleEdit.jsp"),
+			@Result(name = "chart", location = "/admin/chart.jsp")
+		})
 public class UserAction extends BaseAction implements ModelDriven<User> {
-	  
-	private User user = new User();//must new(), let ModelDriven input the value
-
-	@Override
-	public User getModel() {
-		return user;
-	}
 
 	private static final Logger logger = Logger.getLogger(UserAction.class);
 
+	private User user = new User();
+
+	public User getModel() {
+		return user;
+	}
+	
 	private UserServiceI userService;
 
 	public UserServiceI getUserService() {
@@ -46,45 +51,171 @@ public class UserAction extends BaseAction implements ModelDriven<User> {
 		this.userService = userService;
 	}
 
-	public void reg() {
-		Json j = new Json();
-		try {
-			userService.save(user);
-			j.setSuccess(true);
-			j.setMsg("register done");
-		} catch (Exception e) {
-			//j.setMsg(e.getMessage());
-			j.setMsg("register fail");
-		}
-
-		super.writeJson(j);
-
+	public String user() {
+		return "user";
 	}
 
+	public String userAdd() {
+		return "userAdd";
+	}
+
+	public String userEdit() {
+		return "userEdit";
+	}
+
+	public String userRoleEdit() {
+		return "userRoleEdit";
+	}
+	
+	public String chart() {
+		List<RoleChart> datalist = userService.countRole();
+		String filename = ChartUtils.getUserBarChart(datalist);
+		request.setAttribute("filename", filename);
+		return "chart";
+	}
+
+	public void doNotNeedSession_login() {
+		Json j = new Json();
+		User u = userService.login(user);
+		if (u != null) {
+			SessionInfo sessionInfo = new SessionInfo();
+			sessionInfo.setUserId(u.getCid());
+			sessionInfo.setLoginName(user.getCname());
+			sessionInfo.setLoginPassword(user.getCpwd());
+			sessionInfo.setIp(IpUtil.getIpAddr(ServletActionContext.getRequest()));
+			ServletActionContext.getRequest().getSession().setAttribute(ResourceUtil.getSessionInfoName(), sessionInfo);
+			sessionInfo.setAuthIds(u.getAuthIds());
+			sessionInfo.setAuthNames(u.getAuthNames());
+			sessionInfo.setRoleIds(u.getRoleIds());
+			sessionInfo.setRoleNames(u.getRoleNames());
+			sessionInfo.setAuthUrls(u.getAuthUrls());
+
+			j.setObj(sessionInfo);
+			j.setSuccess(true);
+			j.setMsg("Login successfully");
+		} else {
+			j.setMsg("Login unsuccessfully");
+		}
+		super.writeJson(j);
+	}
+
+	public void doNotNeedSession_reg() {
+		Json j = new Json();
+		//Test: can not add admin
+		if(!("admin".equals(user.getCname()))){
+			try {
+				userService.save(user);
+				j.setSuccess(true);
+				j.setMsg("Register successfully");
+			} catch (Exception e) {
+				logger.error(ExceptionUtil.getExceptionMessage(e));
+				j.setMsg("Register unsuccessfully");
+			}
+		}else{
+			logger.error("Can not register admin");
+			j.setMsg("Can not register admin");
+		}
+		super.writeJson(j);
+	}
+
+	public void doNotNeedSession_logout() {
+		ServletActionContext.getRequest().getSession().invalidate();
+		Json j = new Json();
+		j.setSuccess(true);
+		super.writeJson(j);
+	}
+	
 	public void add() {
 		Json j = new Json();
-		try {
-			User u = userService.save(user);
-			j.setSuccess(true);
-			j.setMsg("add user done");
-			j.setObj(u);
-		} catch (Exception e) {
-			//j.setMsg(e.getMessage());
-			j.setMsg("add user fail");
+		//can not add admin
+		if(!("admin".equals(user.getCname()))){
+			try {			
+				userService.save(user);
+				j.setSuccess(true);
+				j.setMsg("Add successfully");
+			} catch (Exception e) {
+				logger.error(ExceptionUtil.getExceptionMessage(e));
+				j.setMsg("Add unsuccessfully");
+			}
+		}else{
+			logger.error("Can not add admin");
+			j.setMsg("Can not add admin");
 		}
 		super.writeJson(j);
 	}
 
-	public void login() {
-		User u = userService.login(user);
+	public void edit() {
 		Json j = new Json();
-		if (u != null) {
-			j.setSuccess(true);
-			j.setMsg("login successful");
-		} else {
-			j.setMsg("login fail, username or password wrong");
+		//can not update admin
+		if(!("0".equals(user.getCid()))){
+			try {
+				userService.update(user);
+				j.setSuccess(true);
+				j.setMsg("Edit successfully");
+			} catch (Exception e) {
+				logger.error(ExceptionUtil.getExceptionMessage(e));
+				j.setMsg("Edit unsuccessfully");
+			}
+		}else{
+			logger.error("Can not update admin");
+			j.setMsg("Can not update admin");
 		}
+		super.writeJson(j);
+	}
+	
+	public void delete() {
+		Json j = new Json();
+		//can not delete admin
+		boolean isAdmin = false;
+		String ids = user.getIds();
+		if(ids!=null){
+			for (String id : ids.split(",")){
+				if (id.trim().equals("0")) {
+					isAdmin = true;
+				}
+			}		
+		}
+		if(!isAdmin){
+			try {
+				userService.delete(user.getIds());
+				j.setSuccess(true);
+				j.setMsg("Delete successfully");
+			}catch (Exception e) {
+				logger.error(ExceptionUtil.getExceptionMessage(e));
+				j.setMsg("Delete unsuccessfully");
+			}
+		}else{
+			logger.error("Can not delete admin");
+			j.setMsg("Can not delete admin");
+		}	
+		super.writeJson(j);
+	}
 
+	public void roleEdit() {
+		Json j = new Json();
+		//can not edit role admin
+		boolean isAdmin = false;
+		String ids = user.getIds();
+		if(ids!=null){
+			for (String id : ids.split(",")){
+				if (id.trim().equals("0")) {
+					isAdmin = true;
+				}
+			}		
+		}
+		if(!isAdmin){
+			try {
+				userService.roleEdit(user);
+				j.setSuccess(true);
+				j.setMsg("Edit role successfully");
+			} catch (Exception e) {
+				logger.error(ExceptionUtil.getExceptionMessage(e));
+				j.setMsg("Edit role unsuccessfully");
+			}
+		}else{
+			logger.error("Can not edit role admin");
+			j.setMsg("Can not edit role admin");
+		}
 		super.writeJson(j);
 	}
 
@@ -92,40 +223,15 @@ public class UserAction extends BaseAction implements ModelDriven<User> {
 		super.writeJson(userService.datagrid(user));
 	}
 
-	public void remove() {
-		userService.remove(user.getIds());
-		Json j = new Json();
-		j.setSuccess(true);
-		j.setMsg("delete done");
-		super.writeJson(j);
+	public void doNotNeedSession_combobox() {
+		super.writeJson(userService.combobox(user));
 	}
-	
-	public void edit() {
-		Json j = new Json();
-		try {
-			User u = userService.edit(user);
-			j.setSuccess(true);
-			j.setMsg("edit user done");
-			j.setObj(u);
-		} catch (Exception e) {
-			j.setMsg("edit user fail");
+
+	public void doNotNeedSession_datagrid() {
+		if (user.getQ() != null && !user.getQ().trim().equals("")) {
+			user.setCname(user.getQ());
 		}
-		super.writeJson(j);
+		super.writeJson(userService.datagrid(user));
 	}
-	
-	public void test(){
-		logger.info("user action test");
-		ServletContext application = ServletActionContext.getRequest().getSession().getServletContext();
-		ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(application);
-		TestService testService = (TestService) context.getBean("testService");
-		System.out.println(testService);
-		testService.test();
-		
-		Tuser t = new Tuser();
-		t.setId(UUID.randomUUID().toString());
-		t.setCreatedatetime(new Date());
-		t.setName("abcdef");
-		t.setPwd("123456");
-		testService.save(t);
-	}
+
 }
